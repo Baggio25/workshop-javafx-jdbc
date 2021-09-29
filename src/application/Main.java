b3a7e4fa1811ba854d2127adbcf18c2b6ae093bc
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-28 22:23:15 -0300

    MainView design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,23 +4,35 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
-			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
-			primaryStage.show();
+			
+			carregaMain(primaryStage);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void carregaMain(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+		ScrollPane scrollPane = loader.load();
+		
+		scrollPane.setFitToHeight(true);
+		scrollPane.setFitToWidth(true);
+		
+		Scene mainScene = new Scene(scrollPane);
+		primaryStage.setScene(mainScene);
+		primaryStage.setTitle("Sample JavaFX application");
+		primaryStage.show();
+		primaryStage.setMaximized(true);
+		primaryStage.centerOnScreen();
 	}
 
 	public static void main(String[] args) {
